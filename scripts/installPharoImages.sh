462219586b51152da705f286c916af0b18363b1f
jordanmontt <sebastianjmt@protonmail.ch>
2026-04-13 17:39:20 +0200

    script

--- scripts/installPharoImages.sh
+++ scripts/installPharoImages.sh
@@ -13,10 +13,11 @@ PHARO_CMD="$BASE_DIR/pharo"
 install_veritas_and_senders_rewriter_for() {
 local image_path="$1"
 local veritas_bench="$2"
-"$PHARO_CMD" --headless "$image_path" metacello install "github://jordanmontt/PharoVeritasBenchSuite:main" "$veritas_bench"
+"$PHARO_CMD" --headless "$image_path" metacello install --save "github://jordanmontt/PharoVeritasBenchSuite:main" "$veritas_bench"
 echo; echo; echo
 echo "Installed Veritas $veritas_bench for image $image_path"
-"$PHARO_CMD" --headless "$image_path" metacello install "github://jordanmontt//path-sensitive-pretenuring:main" "BaselineOfPathSensitivePretenuring"
+
+"$PHARO_CMD" --headless "$image_path" metacello install --save "github://jordanmontt//path-sensitive-pretenuring:main" "PathSensitivePretenuring"
 echo; echo; echo
 echo "Installed Path Sensitive Pretenuring $veritas_bench for image $image_path"
 }
@@ -24,7 +25,7 @@ echo "Installed Path Sensitive Pretenuring $veritas_bench for image $image_path"
 rewrite_senders() {
 local image_path="$1"
 local jsonFileName="$2"
-"$PHARO_CMD" --headless "$image_path" eval "PathSensitvePretenuringExperiment new deserializeSendersToPretenureFrom: $jsonFileName; rewriteAllocationSites"
+"$PHARO_CMD" --headless "$image_path" eval --save "PathSensitvePretenuringExperiment new deserializeSendersToPretenureFrom: '$jsonFileName'; rewriteAllocationSites"
 }
 
 setup_base_image() {
@@ -47,7 +48,7 @@ setup_base_image
 mkdir -p cormas-applicationMethod
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../cormas-applicationMethod/cormas-applicationMethod
 cp "$BASE_DIR"/*.sources ./cormas-applicationMethod/
-install_veritas_and_senders_rewriter_for ./cormas-applicationMethod/cormas-applicationMethod.image BaselineOfVeritasCormas
+install_veritas_and_senders_rewriter_for ./cormas-applicationMethod/cormas-applicationMethod.image VeritasCormas
 mv ./cormas-applicationMethod/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/cormas-applicationMethod.json ./cormas-applicationMethod/
 rewrite_senders ./cormas-applicationMethod/cormas-applicationMethod.image cormas-applicationMethod.json
 echo; echo; echo
@@ -58,7 +59,7 @@ echo "cormas-applicationMethod.json copied"
 mkdir -p cormas-callerOfNew
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../cormas-callerOfNew/cormas-callerOfNew
 cp "$BASE_DIR"/*.sources ./cormas-callerOfNew/
-install_veritas_and_senders_rewriter_for ./cormas-callerOfNew/cormas-callerOfNew.image BaselineOfVeritasCormas
+install_veritas_and_senders_rewriter_for ./cormas-callerOfNew/cormas-callerOfNew.image VeritasCormas
 mv ./cormas-callerOfNew/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/cormas-callerOfNew.json ./cormas-callerOfNew/
 rewrite_senders ./cormas-callerOfNew/cormas-callerOfNew.image cormas-callerOfNew.json
 echo; echo; echo
@@ -69,7 +70,7 @@ echo "cormas-callerOfNew.json copied"
 mkdir -p cormas-locationOfNew
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../cormas-locationOfNew/cormas-locationOfNew
 cp "$BASE_DIR"/*.sources ./cormas-locationOfNew/
-install_veritas_and_senders_rewriter_for ./cormas-locationOfNew/cormas-locationOfNew.image BaselineOfVeritasCormas
+install_veritas_and_senders_rewriter_for ./cormas-locationOfNew/cormas-locationOfNew.image VeritasCormas
 mv ./cormas-locationOfNew/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/cormas-locationOfNew.json ./cormas-locationOfNew/
 rewrite_senders ./cormas-locationOfNew/cormas-locationOfNew.image cormas-locationOfNew.json
 echo; echo; echo
@@ -80,7 +81,7 @@ echo "cormas-locationOfNew.json copied"
 mkdir -p microdown-applicationMethod
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../microdown-applicationMethod/microdown-applicationMethod
 cp "$BASE_DIR"/*.sources ./microdown-applicationMethod/
-install_veritas_and_senders_rewriter_for ./microdown-applicationMethod/microdown-applicationMethod.image BaselineOfVeritasMicrodown
+install_veritas_and_senders_rewriter_for ./microdown-applicationMethod/microdown-applicationMethod.image VeritasMicrodown
 mv ./microdown-applicationMethod/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/microdown-applicationMethod.json ./microdown-applicationMethod/
 rewrite_senders ./microdown-applicationMethod/microdown-applicationMethod.image microdown-applicationMethod.json
 echo; echo; echo
@@ -97,7 +98,7 @@ echo "Spec2Book downloaded for microdown-applicationMethod"
 mkdir -p microdown-callerOfNew
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../microdown-callerOfNew/microdown-callerOfNew
 cp "$BASE_DIR"/*.sources ./microdown-callerOfNew/
-install_veritas_and_senders_rewriter_for ./microdown-callerOfNew/microdown-callerOfNew.image BaselineOfVeritasMicrodown
+install_veritas_and_senders_rewriter_for ./microdown-callerOfNew/microdown-callerOfNew.image VeritasMicrodown
 mv ./microdown-callerOfNew/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/microdown-callerOfNew.json ./microdown-callerOfNew/
 rewrite_senders ./microdown-callerOfNew/microdown-callerOfNew.image microdown-callerOfNew.json
 echo; echo; echo
@@ -114,7 +115,7 @@ echo "Spec2Book downloaded for microdown-callerOfNew"
 mkdir -p microdown-locationOfNew
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../microdown-locationOfNew/microdown-locationOfNew
 cp "$BASE_DIR"/*.sources ./microdown-locationOfNew/
-install_veritas_and_senders_rewriter_for ./microdown-locationOfNew/microdown-locationOfNew.image BaselineOfVeritasMicrodown
+install_veritas_and_senders_rewriter_for ./microdown-locationOfNew/microdown-locationOfNew.image VeritasMicrodown
 mv ./microdown-locationOfNew/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/microdown-locationOfNew.json ./microdown-locationOfNew/
 rewrite_senders ./microdown-locationOfNew/microdown-locationOfNew.image microdown-locationOfNew.json
 echo; echo; echo
@@ -131,7 +132,7 @@ echo "Spec2Book downloaded for microdown-locationOfNew"
 mkdir -p dataframe-applicationMethod
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../dataframe-applicationMethod/dataframe-applicationMethod
 cp "$BASE_DIR"/*.sources ./dataframe-applicationMethod/
-install_veritas_and_senders_rewriter_for ./dataframe-applicationMethod/dataframe-applicationMethod.image BaselineOfVeritasDataFrame
+install_veritas_and_senders_rewriter_for ./dataframe-applicationMethod/dataframe-applicationMethod.image VeritasDataFrame
 mv ./dataframe-applicationMethod/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/dataframe-applicationMethod.json ./dataframe-applicationMethod/
 rewrite_senders ./dataframe-applicationMethod/dataframe-applicationMethod.image dataframe-applicationMethod.json
 echo; echo; echo
@@ -145,7 +146,7 @@ echo "dataset copied for dataframe-applicationMethod"
 mkdir -p dataframe-callerOfNew
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../dataframe-callerOfNew/dataframe-callerOfNew
 cp "$BASE_DIR"/*.sources ./dataframe-callerOfNew/
-install_veritas_and_senders_rewriter_for ./dataframe-callerOfNew/dataframe-callerOfNew.image BaselineOfVeritasDataFrame
+install_veritas_and_senders_rewriter_for ./dataframe-callerOfNew/dataframe-callerOfNew.image VeritasDataFrame
 mv ./dataframe-callerOfNew/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/dataframe-callerOfNew.json ./dataframe-callerOfNew/
 rewrite_senders ./dataframe-callerOfNew/dataframe-callerOfNew.image dataframe-callerOfNew.json
 echo; echo; echo
@@ -159,7 +160,7 @@ echo "dataset copied for dataframe-callerOfNew"
 mkdir -p dataframe-locationOfNew
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../dataframe-locationOfNew/dataframe-locationOfNew
 cp "$BASE_DIR"/*.sources ./dataframe-locationOfNew/
-install_veritas_and_senders_rewriter_for ./dataframe-locationOfNew/dataframe-locationOfNew.image BaselineOfVeritasDataFrame
+install_veritas_and_senders_rewriter_for ./dataframe-locationOfNew/dataframe-locationOfNew.image VeritasDataFrame
 mv ./dataframe-locationOfNew/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/dataframe-locationOfNew.json ./dataframe-locationOfNew/
 rewrite_senders ./dataframe-locationOfNew/dataframe-locationOfNew.image dataframe-locationOfNew.json
 echo; echo; echo
@@ -173,7 +174,7 @@ echo "dataset copied for dataframe-locationOfNew"
 mkdir -p moose-applicationMethod
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../moose-applicationMethod/moose-applicationMethod
 cp "$BASE_DIR"/*.sources ./moose-applicationMethod/
-install_veritas_and_senders_rewriter_for ./moose-applicationMethod/moose-applicationMethod.image BaselineOfVeritasMoose
+install_veritas_and_senders_rewriter_for ./moose-applicationMethod/moose-applicationMethod.image VeritasMoose
 mv ./moose-applicationMethod/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/moose-applicationMethod.json ./moose-applicationMethod/
 rewrite_senders ./moose-applicationMethod/moose-applicationMethod.image moose-applicationMethod.json
 echo; echo; echo
@@ -184,7 +185,7 @@ echo "moose-applicationMethod.json copied"
 mkdir -p moose-callerOfNew
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../moose-callerOfNew/moose-callerOfNew
 cp "$BASE_DIR"/*.sources ./moose-callerOfNew/
-install_veritas_and_senders_rewriter_for ./moose-callerOfNew/moose-callerOfNew.image BaselineOfVeritasMoose
+install_veritas_and_senders_rewriter_for ./moose-callerOfNew/moose-callerOfNew.image VeritasMoose
 mv ./moose-callerOfNew/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/moose-callerOfNew.json ./moose-callerOfNew/
 rewrite_senders ./moose-callerOfNew/moose-callerOfNew.image moose-callerOfNew.json
 echo; echo; echo
@@ -195,7 +196,7 @@ echo "moose-callerOfNew.json copied"
 mkdir -p moose-locationOfNew
 "$PHARO_CMD" --headless "$BASE_IMAGE_FILE" save ../moose-locationOfNew/moose-locationOfNew
 cp "$BASE_DIR"/*.sources ./moose-locationOfNew/
-install_veritas_and_senders_rewriter_for ./moose-locationOfNew/moose-locationOfNew.image BaselineOfVeritasMoose
+install_veritas_and_senders_rewriter_for ./moose-locationOfNew/moose-locationOfNew.image VeritasMoose
 mv ./moose-locationOfNew/pharo-local/iceberg/jordanmontt/path-sensitive-pretenuring/files/paths-to-pretenure/moose-locationOfNew.json ./moose-locationOfNew/
 rewrite_senders ./moose-locationOfNew/moose-locationOfNew.image moose-locationOfNew.json
 echo; echo; echo
